8505c0c2310de52b7309f33f28a6101315ba1d97
Ediongsenyene Joseph <ediongi6@hotmail.com>
2022-11-26 19:04:16 +0100

    Add google fonts

--- todo-app/tailwind.config.js
+++ todo-app/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./index.html', './scripts/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mobile-lg': "url('/images/bg-mobile-light.jpg')",
+      },
+      fontFamily: {
+        sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
