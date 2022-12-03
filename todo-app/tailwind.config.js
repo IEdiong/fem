38295a822f2e48ba6ee9862b5289faab64282055
Ediongsenyene Joseph <ediongi6@hotmail.com>
2022-12-03 16:15:25 +0100

    Add custom checkbox--one

--- todo-app/tailwind.config.js
+++ todo-app/tailwind.config.js
@@ -5,11 +5,20 @@ module.exports = {
   content: ['./index.html', './scripts/*.js'],
   theme: {
     extend: {
+      colors: {
+        'bright-blue': 'hsl(220, 98%, 61%)',
+        'check-start': 'hsl(192, 100%, 67%)',
+        'check-end': 'hsl(280, 87%, 65%)',
+      },
       backgroundImage: {
         'mobile-lg': "url('/images/bg-mobile-light.jpg')",
+        'icon-check': "url('/images/icon-check.svg')",
       },
       fontFamily: {
         sans: ['Josefin Sans', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        '3xl': '0px 5px 30px 1px rgba(0,0,0,0.1)',
       },
     },
   },
