08f5b4c06953740089bb1ffd72e1bf47e5bdf10e
Ediongsenyene Joseph <ediongi6@hotmail.com>
2022-12-16 20:03:50 +0100

    Add function to add todo item

--- todo-app/src/scripts/main.js
+++ todo-app/src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('Script connected');
+// Step 01: Grab elements
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+// Step 02: Add event listeners
+window.addEventListener('keypress', addTodo);
+
+// Step 03: Create callback functions
+function addTodo(e) {
+  //   console.log(e);
+
+  const item = todoInput.value;
+  if (e.keyCode === 13 && item) {
+    e.preventDefault();
+    const html = `
+        <li class="todo px-3 py-4 w-full flex items-center gap-x-2 text-dark-gray-100">
+        <!-- TODO: fix the focus state -->
+        <input
+          type="checkbox"
+          id="checkbox1"
+          class="peer appearance-none relative w-5 h-5 rounded-full focus:outline-none shrink-0 before:content-[''] before:w-[22px] before:h-[22px] before:-left-[1px] before:-top-[1px] before:bg-check-gray checked:before:bg-gradient-to-br hover:before:bg-gradient-to-br checked:before:from-check-start checked:before:to-check-end hover:from-check-start hover:to-check-end before:absolute before:rounded-full after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:bg-white checked:after:bg-center checked:after:bg-no-repeat checked:after:bg-transparent checked:after:bg-[url('/images/icon-check.svg')] after:absolute after:rounded-full cursor-pointer ease-in transition-all"
+        />
+        <label for="checkbox1" class="w-full md:cursor-pointer">${item}</label>
+        <button class="bg-transparent">
+          <img src="/images/icon-cross.svg" alt="" />
+        </button>
+      </li>
+        `;
+
+    todoList.insertAdjacentHTML('afterbegin', html);
+
+    todoInput.value = '';
+  }
+}
