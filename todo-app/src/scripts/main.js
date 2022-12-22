94d8a2d010fef6cd2575ca299c697c814329862d
Ediongsenyene Joseph <ediongi6@hotmail.com>
2022-12-22 12:01:02 +0100

    Add functionality to delete todos

--- todo-app/src/scripts/main.js
+++ todo-app/src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// App State
 // const todos = [
 //   { id: 1, title: 'Complete Todo App on Frontend Mentor', done: false },
 //   { id: 2, title: 'Pick up groceries', done: true },
@@ -15,14 +16,11 @@ const form = document.getElementById('todo-form');
 // Step 02: Add event listeners
 // window.addEventListener('DOMContentLoaded', addTodo);
 form.addEventListener('submit', addTodo);
+todoList.addEventListener('click', checkDelete);
 
 // Step 03: Create callback functions
 function addTodo(e) {
-  //   console.log(e);
-
   const item = todoInput.value;
-  // if (e.keyCode === 13 && item) {
-  // });
   e.preventDefault();
   if (item) {
     const html = `
@@ -34,8 +32,8 @@ function addTodo(e) {
           class="peer appearance-none relative w-5 h-5 rounded-full focus:outline-none shrink-0 before:content-[''] before:w-[22px] before:h-[22px] before:-left-[1px] before:-top-[1px] before:bg-check-gray checked:before:bg-gradient-to-br hover:before:bg-gradient-to-br checked:before:from-check-start checked:before:to-check-end hover:from-check-start hover:to-check-end before:absolute before:rounded-full after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:bg-white checked:after:bg-center checked:after:bg-no-repeat checked:after:bg-transparent checked:after:bg-[url('/images/icon-check.svg')] after:absolute after:rounded-full cursor-pointer ease-in transition-all"
         />
         <label for="checkbox-${todoId}" class="w-full md:cursor-pointer">${item}</label>
-        <button class="bg-transparent">
-          <img src="/images/icon-cross.svg" alt="" />
+        <button id="del" class="bg-transparent">
+          <img class="pointer-events-none" src="/images/icon-cross.svg" alt="" />
         </button>
       </li>
         `;
@@ -45,26 +43,34 @@ function addTodo(e) {
     todoInput.value = '';
     todoId++;
   }
-
-  // todos.map((todo) => {
-  //   const html = `
-  //       <li class="todo px-3 py-4 w-full flex items-center gap-x-2 text-dark-gray-100">
-  //       <!-- TODO: fix the focus state -->
-  //       <input
-  //         type="checkbox"
-  //         id="checkbox-${todo.id}"
-  //         ${todo.done ? 'checked' : ''}
-  //         class="peer appearance-none relative w-5 h-5 rounded-full focus:outline-none shrink-0 before:content-[''] before:w-[22px] before:h-[22px] before:-left-[1px] before:-top-[1px] before:bg-check-gray checked:before:bg-gradient-to-br hover:before:bg-gradient-to-br checked:before:from-check-start checked:before:to-check-end hover:from-check-start hover:to-check-end before:absolute before:rounded-full after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:bg-white checked:after:bg-center checked:after:bg-no-repeat checked:after:bg-transparent checked:after:bg-[url('/images/icon-check.svg')] after:absolute after:rounded-full cursor-pointer ease-in transition-all"
-  //       />
-  //       <label for="checkbox-${todo.id}" class="w-full md:cursor-pointer">${todo.title}</label>
-  //       <button class="bg-transparent">
-  //         <img src="/images/icon-cross.svg" alt="" />
-  //       </button>
-  //     </li>
-  //       `;
-
-  //   todoList.insertAdjacentHTML('afterbegin', html);
-
-  //   todoInput.value = '';
-  // });
 }
+
+function checkDelete(e) {
+  const elClicked = e.target;
+
+  if (elClicked.id === 'del') {
+    elClicked.parentElement.remove();
+  }
+}
+
+// todos.map((todo) => {
+//   const html = `
+//       <li class="todo px-3 py-4 w-full flex items-center gap-x-2 text-dark-gray-100">
+//       <!-- TODO: fix the focus state -->
+//       <input
+//         type="checkbox"
+//         id="checkbox-${todo.id}"
+//         ${todo.done ? 'checked' : ''}
+//         class="peer appearance-none relative w-5 h-5 rounded-full focus:outline-none shrink-0 before:content-[''] before:w-[22px] before:h-[22px] before:-left-[1px] before:-top-[1px] before:bg-check-gray checked:before:bg-gradient-to-br hover:before:bg-gradient-to-br checked:before:from-check-start checked:before:to-check-end hover:from-check-start hover:to-check-end before:absolute before:rounded-full after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:bg-white checked:after:bg-center checked:after:bg-no-repeat checked:after:bg-transparent checked:after:bg-[url('/images/icon-check.svg')] after:absolute after:rounded-full cursor-pointer ease-in transition-all"
+//       />
+//       <label for="checkbox-${todo.id}" class="w-full md:cursor-pointer">${todo.title}</label>
+//       <button class="bg-transparent">
+//         <img src="/images/icon-cross.svg" alt="" />
+//       </button>
+//     </li>
+//       `;
+
+//   todoList.insertAdjacentHTML('afterbegin', html);
+
+//   todoInput.value = '';
+// });
