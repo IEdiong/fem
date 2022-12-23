a6a59bdf098507b2711ec3661d0d45d335653514
Ediongsenyene Joseph <ediongi6@hotmail.com>
2022-12-24 00:45:22 +0100

    Add fixes for html accessibility

--- todo-app/src/scripts/theme.js
+++ todo-app/src/scripts/theme.js
@@ -3,8 +3,10 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
   document.documentElement.classList.add('dark');
+  document.getElementById('theme-toggle')?.setAttribute('aria-label', 'light-theme');
 } else {
   document.documentElement.classList.remove('dark');
+  document.getElementById('theme-toggle')?.setAttribute('aria-label', 'dark-theme');
 }
 
 document.getElementById('theme-toggle').addEventListener('click', () => {
@@ -12,9 +14,11 @@ document.getElementById('theme-toggle').addEventListener('click', () => {
     // Whenever the user explicitly chooses light mode
     localStorage.theme = 'light';
     document.documentElement.classList.remove('dark');
+    document.getElementById('theme-toggle')?.setAttribute('aria-label', 'dark-theme');
   } else {
     // Whenever the user explicitly chooses dark mode
     localStorage.theme = 'dark';
     document.documentElement.classList.add('dark');
+    document.getElementById('theme-toggle')?.setAttribute('aria-label', 'light-theme');
   }
 });
