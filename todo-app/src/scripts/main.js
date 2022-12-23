ab3ba1142fcb76d1204d531fd50a3de2802b25e6
Ediongsenyene Joseph <ediongi6@hotmail.com>
2022-12-23 17:21:09 +0100

    Add todo list styles for desktop

--- todo-app/src/scripts/main.js
+++ todo-app/src/scripts/main.js
@@ -148,7 +148,7 @@ function removeLocalTodo(todo) {
 // Render todo element to the DOM
 function renderTodos(note, completed) {
   const html = `
-        <li class="todo px-3 py-4 w-full flex items-center gap-x-2 text-dark-gray-100" data-todo-item >
+        <li class="todo group px-3 md:px-6 py-4 w-full flex items-center gap-x-2 md:gap-x-6 text-dark-gray-100" data-todo-item >
         <!-- TODO: fix the focus state -->
         <input
           type="checkbox"
@@ -157,7 +157,7 @@ function renderTodos(note, completed) {
           class="peer appearance-none relative w-5 h-5 rounded-full focus:outline-none shrink-0 before:content-[''] before:w-[22px] before:h-[22px] before:-left-[1px] before:-top-[1px] before:bg-check-gray checked:before:bg-gradient-to-br hover:before:bg-gradient-to-br checked:before:from-check-start checked:before:to-check-end hover:from-check-start hover:to-check-end before:absolute before:rounded-full after:content-[''] after:w-full after:h-full after:left-0 after:top-0 after:bg-white checked:after:bg-center checked:after:bg-no-repeat checked:after:bg-transparent checked:after:bg-[url('/images/icon-check.svg')] after:absolute after:rounded-full cursor-pointer ease-in transition-all"
         />
         <label for="checkbox-${todoId}" class="w-full md:cursor-pointer ${completed ? 'line-through text-dark-gray-50' : ''}">${note}</label>
-        <button id="del" class="bg-transparent">
+        <button id="del" class="bg-transparent md:opacity-0 group-hover:opacity-100">
           <img class="pointer-events-none" src="/images/icon-cross.svg" alt="" />
         </button>
       </li>
